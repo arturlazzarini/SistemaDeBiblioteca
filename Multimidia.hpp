7fe36555bfb44ae08d01ca541e21f418d0c3e006
arturlazzarini <42216694+arturlazzarini@users.noreply.github.com>
2018-11-14 21:19:14 -0200

    Create Multimidia.hpp

---
+++ Multimidia.hpp
@@ -0,0 +1,16 @@
+#ifndef MULTIMIDIA_HPP
+#define MULTIMIDIA_HPP
+
+class Multimidia : public Item{
+	private:
+		int _itensDisponiveis = 0;
+		string _tipoMultimidia;
+		
+	public:
+		void setItensDisponiveis(int quantidade){}
+		void setTipoMultimidia(string tipo){}
+
+}
+
+
+#endif
