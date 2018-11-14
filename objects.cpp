eb1b4ecb05cf76b27bc3033db618f6c880f63c57
arturlazzarini <42216694+arturlazzarini@users.noreply.github.com>
2018-11-14 13:15:17 -0200

    Update objects

--- objects.cpp
+++ objects.cpp
@@ -1,18 +1,49 @@
-
 class Pessoa{
 	private:
 		string _nome;
-		string senha;
-		int id;
-
+		string _senha;
+		int _id = 0;
+	
+	public:
+		Pessoa(string nome, string senha, string id){
+			setNome(nome);
+			setSenha(senha);
+			setId();
+			
+		}
+	
+		void setNome(string nome){
+			_nome = nome;
+		}
+		
+		void setSenha(string senha){
+			_senha = senha;
+		}
+		
+		void setId(){
+			
+		}
+		
+		string getNome(){
+			return _nome;
+		}
+		
+		string getSenha(){
+			return _senha;
+		}
+		
+		string getId(){
+		
+		}
+	
 }
 
 class Usuario : public Pessoa{
 
 	public:
-		void alugarItem(string item){};
-		void reservarItem(string item){};
-		void devolverItem(string item){};
+		void alugarItem(string item){}
+		void reservarItem(string item){}
+		void devolverItem(string item){}
 
 }
 
@@ -21,7 +52,10 @@ class Funcionario : public Pessoa{
 	public:
 		void adicionarItem(string titulo, string autor, string prod){};
 		void removerItem(string item){};
-		void cadastrarUsuario(string nome, string senha, string id){};
+		void cadastrarUsuario(Gerencia adm, string nome, string senha, string id){
+			Pessoa novo(nome, senha, id);
+			adm.setUsuariosCadastrados(novo);
+		};
 
 }
 
@@ -32,14 +66,68 @@ class Item{
 		string _titulo;
 		string _autor;
 		string _prod;
-		int _codigo;
-		int _quantidadeItens;
+		int _codigo = 0;
+		int _quantidadeItens = 0;
+		
+	public:
+		Item(string titulo, string autor, string prod, int codigo, int quantidade){
+			setTitulo(titulo);
+			setAutor(autor);
+			setProd(prod);
+			setCodigo(codigo);
+			setQuantidadeItens(quantidade);
+		}
+
+		void setTitulo(string titulo){
+			_titulo = titulo;
+		}
+		
+		void setAutor(string autor){
+			_autor = autor;
+		}
+		
+		void setProd(string produtora){
+			_prod = produtora;
+		}
+		
+		void setCodigo(){
+		
+		}
+		
+		void setQuantidadeItens(int quantidade){
+			_quantidadeItens += quantidade;
+		}
+		
+		string getTitulo(){
+			return _titulo;
+		}
+		
+		string getAutor(){
+			return _autor;
+		}
+		
+		string getProd(){
+			return _prod;
+		}
+		
+		int getCodigo(){
+			return _codigo;
+		}
+		
+		int getQuantidadeItens(){
+			return _quantidadeItens;
+		}
 
 }
 
 class Livro : public Item{
 	private:
-		int _itensDisponiveis;
+		int _itensDisponiveis = 0;
+
+	public:
+		void setItensDisponiveis(int quantidade){
+			_itensDisponiveis = quantidade;
+		}
 
 }
 
@@ -52,6 +140,14 @@ class Multimidia : public Item{
 
 
 class Gerencia{
+	private:
+		std::vector <Pessoa> _usuariosCadastrados;
+		
+	public:
+		void setUsuariosCadastrados(Pessoa novoUsuario){
+			_usuariosCadastrados.push_back(novoUsuario);
+		}
+
 
 }
 
@@ -63,5 +159,4 @@ class Acervo{
 		void listaItens(){};
 		void procuraItens(){};
 }
-
 
