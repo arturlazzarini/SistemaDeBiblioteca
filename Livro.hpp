d4906f453bb2a8352a5e7091e8df9ffa9e1ecce2
arturlazzarini <42216694+arturlazzarini@users.noreply.github.com>
2018-11-14 21:14:15 -0200

    Create Livro.hpp

---
+++ Livro.hpp
@@ -0,0 +1,15 @@
+#ifndef LIVRO_HPP
+#define LIVRO_HPP
+
+#include "Item.hpp"
+
+class Livro : public Item{
+	private:
+		int _itensDisponiveis;
+
+	public:
+		void setItensDisponiveis(int quantidade){}
+
+}
+
+#endif
