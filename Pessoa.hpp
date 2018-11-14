11e550becf8278d846cd7da9787bccb9788a290e
gabcerqueira <43377719+gabcerqueira@users.noreply.github.com>
2018-11-14 16:52:10 -0200

    pessoa hpp e pessoa cpp
    hpp e cpp de pessoa , seguindo como modelo o objects.cpp

---
+++ Pessoa.hpp
@@ -0,0 +1,46 @@
+#ifndef PESSOA_HPP
+#define PESSOA_HPP
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+
+class Pessoa{
+	private:
+		string _nome;
+		string _senha;
+		int _id = 0;
+	
+	public:
+		Pessoa(string, string, int){
+
+	
+		void setNome(string nome);
+
+		
+		void setSenha(string senha);
+
+		
+		void setId(int id);
+
+		
+		string getNome();
+
+		
+		string getSenha();
+
+		
+		string getId();
+
+}
+
+
+
+
+
+
+
+
+#endif
