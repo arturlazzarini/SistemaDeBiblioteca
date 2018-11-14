23c408d8e3367f3886c95182f8841c14c9fc73b5
arturlazzarini <42216694+arturlazzarini@users.noreply.github.com>
2018-11-14 21:06:20 -0200

    Create Funcionario.cpp

---
+++ Funcionario.cpp
@@ -0,0 +1,14 @@
+#include "Funcionario.hpp"
+#include "Gerencia.hpp"
+
+class Funcionario : public Pessoa{
+
+	public:
+		void adicionarItem(string titulo, string autor, string prod){}
+		void removerItem(string item){}  
+		void cadastrarUsuario(Gerencia adm, string nome, string senha, string id){
+			Pessoa novo(nome, senha, id);
+			adm.setUsuariosCadastrados(novo);
+		}
+
+}
