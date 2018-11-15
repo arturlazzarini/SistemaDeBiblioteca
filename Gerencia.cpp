54e32300d20d5f2afafe6d1be36c54620f789df9
arthurrlisboa <42387780+arthurrlisboa@users.noreply.github.com>
2018-11-15 21:33:23 -0200

    Add files via upload

---
+++ Gerencia.cpp
@@ -0,0 +1,35 @@
+#include "Gerencia.hpp"
+
+Gerencia::Gerencia(){}
+
+void Gerencia::setUsuarioCadastrado(Usuario novo){
+	
+	_usuariosCadastrados.push_back(novo);
+	cout<<"Usuario cadastrado com sucesso"<<endl;
+}
+
+void Gerencia::setFuncionarioCadastrado(Funcionario novo){
+	
+	_funcionariosCadastrados.push_back(novo);
+	cout<<"Funcionario cadastrado com sucesso"<<endl;
+}
+
+int Gerencia::pesquisaFuncionario(Pessoa *funcionario){
+	
+	for(int i=0; i < _funcionariosCadastrados.size(); i++){
+		
+		if(_funcionariosCadastrados[i].getId() == funcionario->getId() && _funcionariosCadastrados[i].getSenha() == funcionario->getSenha())
+			return 1;	
+	}
+	return 0;
+}
+
+int Gerencia::pesquisaUsuario(Pessoa *usuario){
+	
+	for(int i=0; i < _usuariosCadastrados.size(); i++){
+		
+		if(_usuariosCadastrados[i].getId() == usuario->getId() && _usuariosCadastrados[i].getSenha() == usuario->getSenha())
+			return 1;	
+	}
+	return 0;	
+}
