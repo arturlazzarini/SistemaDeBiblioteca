b747cd9e60c8b8cb1b63b4961d818cacf7aa8e46
arthurrlisboa <42387780+arthurrlisboa@users.noreply.github.com>
2018-11-15 20:17:37 -0200

    Update Item.hpp

--- Item.hpp
+++ Item.hpp
@@ -1,62 +1,36 @@
 #ifndef ITEM_HPP
 #define ITEM_HPP
 
+#include <stdbool.h>
+#include <string>
+#include <iostrem>
 
-
+using std::cout
+using std::cin
+using std::string
 
 class Item{
+
 	private:
+	
 		string _titulo;
 		string _autor;
 		string _editora;
-		int _codigo = 0;
-		int _quantidadeItens = 0;
+		bool _disponibilidade;
 		
 	public:
-		Item(string titulo, string autor, string editora, int codigo, int quantidade){
-			setTitulo(titulo);
-			setAutor(autor);
-			setEditora(editora);
-			setCodigo(codigo);
-			setQuantidadeItens(quantidade);
-		}
-
-		void setTitulo(string titulo);
-
-		
-		void setAutor(string autor);
-
-		
-		void setEditora(string editora);
-
-		
-		void setCodigo(codigo);
-
-		
-		void setQuantidadeItens(int quantidade);
-
-		
-		string getTitulo();
-
-		
-		string getAutor();
-
-		
-		string getEditora();
-
-		
-		int getCodigo();
-
-		
-		int getQuantidadeItens();
 	
-		void imprimeItem();
+		Item(string,string,string,int,int);
+		void setTitulo(string);
+		void setAutor(string autor);	
+		void setEditora(string);				
+		string getTitulo();		
+		string getAutor();		
+		string getEditora();			
+		virtual void imprimeCampos();
+		virtual bool isDisponivel();
 
 
-
-}
-
-
+};
 
 #endif
-
