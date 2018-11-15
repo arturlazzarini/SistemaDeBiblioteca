3357b316989d5b0afc702d15cd3f26cecfbe20ff
arthurrlisboa <42387780+arthurrlisboa@users.noreply.github.com>
2018-11-15 21:33:41 -0200

    Add files via upload

---
+++ Gerencia.hpp
@@ -0,0 +1,29 @@
+#ifndef GERENCIA_HPP
+#define GERENCIA_HPP
+
+#include "Pessoa.hpp"
+#include "Usuario.hpp"
+#include "funcionario.hpp"
+#include "Acervo.hpp"
+#include <vector>
+
+using std::vector;
+using std::cout;
+using std::cin;
+
+class Gerencia{
+	
+	private:
+		vector <Usuarios> _usuariosCadastrados;
+		vector <Funcionarios> _funcionariosCadastrados;
+		
+	public:
+		Gerencia();
+		void setUsuarioCadastrado(Usuario);
+		void setFuncionarioCadastrado(Funcionario);
+		int pesquisaFuncionario(Pessoa*);
+		int pesquisaUsuario(Pessoa*);
+
+};
+
+#endif
