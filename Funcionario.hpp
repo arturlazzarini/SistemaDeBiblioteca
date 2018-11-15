ccd8b3397fe9e3da6bb2511b9319b33979c18f0f
gabcerqueira <43377719+gabcerqueira@users.noreply.github.com>
2018-11-15 18:06:44 -0200

    Update Funcionario.hpp

--- Funcionario.hpp
+++ Funcionario.hpp
@@ -1,11 +1,14 @@
 #ifndef FUNCIONARIO_HPP
 #define FUNCIONARIO_HPP
 
+#include "Pessoa.hpp"
+#include "Item.hpp"
+
 class Funcionario : public Pessoa{
 
     public:
-		void adicionarItem(string titulo, string autor, string prod){};
-		void removerItem(string item){};
+		void adicionarItem(Item x);
+		void removerItem(Item x);
 		void cadastrarUsuario(Gerencia adm, string nome, string senha, string id){
 			Pessoa novo(nome, senha, id);
 			adm.setUsuariosCadastrados(novo);
