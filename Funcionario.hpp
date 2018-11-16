38847ddc67572336c2931dd6a672f863f7e11896
arthurrlisboa <42387780+arthurrlisboa@users.noreply.github.com>
2018-11-15 23:24:08 -0200

    Update Funcionario.hpp

--- Funcionario.hpp
+++ Funcionario.hpp
@@ -2,13 +2,14 @@
 #define FUNCIONARIO_HPP
 
 #include "Gerencia.hpp"
-#include "Usuario.hpp"
+#include "Pessoa.hpp"
 #include "Acervo.hpp"
 
 class Funcionario : public Pessoa{
 
     public:
-	
+		Funcionario();
+		Funcionario(string,string,id);
 		void adicionarItem(Acervo *acervo,string titulo, string autor, string prod);
 		void removerItem(Acervo *acervo,string item);
 		void cadastrarUsuario(Gerencia *adm, string nome, string senha, string id);
