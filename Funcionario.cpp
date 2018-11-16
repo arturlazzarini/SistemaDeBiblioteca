0b8dcdb584b270bb9aba5e5dd6558cdb742b8636
arthurrlisboa <42387780+arthurrlisboa@users.noreply.github.com>
2018-11-15 23:23:37 -0200

    Update Funcionario.cpp

--- Funcionario.cpp
+++ Funcionario.cpp
@@ -1,25 +1,28 @@
 #include "Funcionario.hpp"
 
-//Adiciona novos itens ao acervo da biblioteca
+Funcionario :: Funcionario(){}
 
-void Funcionario::adicionarlivro(Acervo *acervo,string titulo, string autor, string _editora){
+Funcionario :: Funcionario(string nome, string senha, int id) : Pessoa(nome,senha,id){}
+
+//Adiciona novos itens ao acervo da biblioteca
+void Funcionario::adicionarlivro(Acervo *acervo,string titulo, string autor, string prod){
 	
-	Livro novo(string titulo, string autor, string _editora);
+	Livro novo(string titulo, string autor, string prod);
 	acervo->adicionarlivro(novo);
 }
 
 //Remove novos itens ao acervo da biblioteca
-void Funcionario::removerLivro(Acervo *acervo,string titulo, string autor, string _editora){
+void Funcionario::removerLivro(Acervo *acervo,string titulo, string autor, string prod){
 	
-	Livro livro(string titulo, string autor, string _editora);
-	acervo->removerLivro(livro);
+	Livro livro(string titulo, string autor, string prod);
+	acervo->removerLivro(livro)
 }
 
 //Cadastra novos usuários e os adiciona à gerencia
 void Funcionario::cadastrarUsuario(Gerencia *adm, string nome, string senha, string id){
 	
 	Usuario novo(nome, senha, id);
-	adm->setUsuarioCadastrado(novo);
+	adm->setUsuariosCadastrados(novo);
 }
 
 //Cadastra novos usuários e os adiciona à gerencia
