208303427d740e609fc512f48063d72be807183a
arthurrlisboa <42387780+arthurrlisboa@users.noreply.github.com>
2018-11-16 18:58:38 -0200

    Update Funcionario.hpp

--- Funcionario.hpp
+++ Funcionario.hpp
@@ -12,10 +12,10 @@ class Funcionario : public Pessoa{
 		Funcionario(string,string,id);
 		void adicionarLivros(Acervo*,string,string,string);
 		void removerLivros(Acervo*,string);
-		void adicionarMultimidias(Acervo*,string,string,string);
-		void removerMultimidias(Acervo*,string);
-		void cadastrarUsuario(Gerencia*, string, string, int id);
-		void cadastrarFuncionario(Gerencia*, string, string, int id);
+		void adicionarMultimidias(Acervo*,string,string,string,string,bool);
+		void removerMultimidias(Acervo*,string,string,string,string,bool);
+		void cadastrarUsuario(Gerencia *adm, string nome, string senha, string id);
+		void cadastrarFuncionario(Gerencia *adm, string nome, string senha, string id);
 };
 
 #endif
