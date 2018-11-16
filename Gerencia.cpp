d8bd9056bf9f36863b783e81155abdad32654b00
arthurrlisboa <42387780+arthurrlisboa@users.noreply.github.com>
2018-11-16 12:59:29 -0200

    Update Gerencia.cpp

--- Gerencia.cpp
+++ Gerencia.cpp
@@ -18,8 +18,10 @@ int Gerencia::pesquisaFuncionario(Pessoa *funcionario){
 	
 	for(int i=0; i < _funcionariosCadastrados.size(); i++){
 		
-		if(_funcionariosCadastrados[i].getId() == funcionario->getId() && _funcionariosCadastrados[i].getSenha() == funcionario->getSenha())
+		if(_funcionariosCadastrados[i].getId() == funcionario->getId() && _funcionariosCadastrados[i].getSenha() == funcionario->getSenha()){
+			funcionario->setNome(_funcionariosCadastrados[i].getNome());
 			return 1;	
+		}
 	}
 	return 0;
 }
@@ -28,8 +30,10 @@ int Gerencia::pesquisaUsuario(Pessoa *usuario){
 	
 	for(int i=0; i < _usuariosCadastrados.size(); i++){
 		
-		if(_usuariosCadastrados[i].getId() == usuario->getId() && _usuariosCadastrados[i].getSenha() == usuario->getSenha())
-			return 1;	
+		if(_usuariosCadastrados[i].getId() == usuario->getId() && _usuariosCadastrados[i].getSenha() == usuario->getSenha()){
+			usuario->setNome(_usuariosCadastrados[i].getNome());
+			return 1;
+		}			
 	}
 	return 0;	
 }
