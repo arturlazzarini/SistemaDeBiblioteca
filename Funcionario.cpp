79ab28486f2084eaeb7fd3c043dd5cb558a2dff4
arthurrlisboa <42387780+arthurrlisboa@users.noreply.github.com>
2018-11-16 18:58:10 -0200

    Update Funcionario.cpp

--- Funcionario.cpp
+++ Funcionario.cpp
@@ -7,32 +7,28 @@ Funcionario :: Funcionario(string nome, string senha, int id) : Pessoa(nome,senh
 //Adiciona novos livros ao acervo da biblioteca
 void Funcionario::adicionarlivros(Acervo *acervo,string titulo, string autor, string prod){
 	
-	Livro novo(string titulo, string autor, string prod);
+	Livro novo(titulo,autor,prod,true);
 	acervo->adicionarlivro(novo);
 }
 
 //Remove novos livros do acervo da biblioteca
 void Funcionario::removerLivros(Acervo *acervo,string titulo, string autor, string prod){
 	
-	Livro livro(string titulo, string autor, string prod);
+	Livro livro(titulo,autor, prod,true);
 	acervo->removerLivro(livro);
 }
 //Adiciona novas multimidias ao acervo da biblioteca
-void Funcionario::adicionarMultimidias(Acervo *acervo,string titulo, string autor){
+void Funcionario::adicionarMultimidias(Acervo *acervo,string titulo, string autor, string produtora, string tipo){
 	
-	Multimidia nova;
-	nova.setTitulo(titulo);
-	nova.setAutor(autor);
+	Multimidia nova(titulo,autor,produtora,tipo,true);
 	acervo->adicionarMultimidia(nova);
 }
 
 //Remove multimidia do acervo da biblioteca
-void Funcionario::removerMultimidias(Acervo *acervo,string titulo, string autor){
+void Funcionario::removerMultimidias(Acervo *acervo,string titulo, string autor,string produtora, string tipo){
 	
-	Multimidia multimidia;
-	multimidia.setTitulo(titulo);
-	multimidia.setAutor(autor);
-	acervo->adicionarMultimidia(multimidia);
+	Multimidia retirada(titulo,autor,produtora,tipo,true);
+	acervo->adicionarMultimidia(retirada);
 }
 
 //Cadastra novos usuários e os adiciona à gerencia
