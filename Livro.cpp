a86419f9f717038a40265caa5abf73ea7366c391
arthurrlisboa <42387780+arthurrlisboa@users.noreply.github.com>
2018-11-16 19:11:01 -0200

    Update Livro.cpp

--- Livro.cpp
+++ Livro.cpp
@@ -4,10 +4,6 @@ Livro::Livro(){}
 
 Livro::Livro(string titulo, string autor, string editora, bool disponibilidade) : Item(titulo,autor,editora,disponibilidade){}
 
-void Livro::setItensDisponiveis(int quantidade){
-	_itensDisponiveis = quantidade;
-}
-
 bool Livro::isDisponivel(){
 	return this->_disponivel;
 }
@@ -20,8 +16,4 @@ void Livro::imprimeCampos(){
 	cout<<"Título : "<<getTitulo()<<endl;
 	cout<<"Autor : "<<getAutor()<<endl;
 	cout<<"Editora : "<<getEditora()<<endl;
-}
-
-void Livro::reservar(){
-	_disponibilidade = false;
 }
