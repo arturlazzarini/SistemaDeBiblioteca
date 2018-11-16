b1660509bc59e030d51d789fc55d84b2d6d27091
arthurrlisboa <42387780+arthurrlisboa@users.noreply.github.com>
2018-11-16 18:54:42 -0200

    Update Multimidia.hpp

--- Multimidia.hpp
+++ Multimidia.hpp
@@ -2,21 +2,23 @@
 #define MULTIMIDIA_HPP
 
 #include "Item.hpp"
+#include <string>
+
+using std::string;
+using std::cout;
 
 class Multimidia : public Item{
+	
 	private:
-
-		bool _disponibilidade = true;
-		
+		string _tipoMultimidia;
+	
 	public:
 
 		Multimidia();
-		Multimidia(string,string,string,bool);
-		void setItensDisponiveis(int);
+		Multimidia(string,string,string,string,bool);
 		bool isDisponivel();
 		void mudarDisponibilidade();
 		void imprimeCampos();
-		void reservar();
 }
 
 #endif
