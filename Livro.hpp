6a03d7a25d253b52bba0ae158379645004f08617
arthurrlisboa <42387780+arthurrlisboa@users.noreply.github.com>
2018-11-16 19:09:55 -0200

    Update Livro.hpp

--- Livro.hpp
+++ Livro.hpp
@@ -12,11 +12,9 @@ class Livro : public Item{
 
 		Livro();
 		Livro(string,string,string,bool);
-		void setItensDisponiveis(int);
 		bool isDisponivel();
 		void mudarDisponibilidade();
 		void imprimeCampos();
-		void reservar();
 }
 
 #endif
