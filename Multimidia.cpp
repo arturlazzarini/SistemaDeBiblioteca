605c630593100140a8f8437a65cf3391967190cf
guilhermebernardo <42388227+guilhermebernardo@users.noreply.github.com>
2018-11-15 23:04:44 -0200

    Update Multimidia.cpp

--- Multimidia.cpp
+++ Multimidia.cpp
@@ -12,6 +12,10 @@ bool Multimidia::isDisponivel(){
 	return this->_disponivel;
 }
 
+void Multimidia::mudarDisponibilidade(){
+	_disponibilidade = true ? _disponibilidade = false :  _disponibilidade = true;
+}
+
 void Multimidia::imprimeCampos(){
 
 	cout<<"Título : "<<getTitulo()<<endl;
