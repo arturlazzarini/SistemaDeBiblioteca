c31c8606f0a281e00ad8ee0da43d44334c9bb8a3
gabcerqueira <43377719+gabcerqueira@users.noreply.github.com>
2018-11-18 21:24:51 -0200

    Update Gerencia.hpp

--- Gerencia.hpp
+++ Gerencia.hpp
@@ -1,6 +1,14 @@
 #ifndef GERENCIA_HPP
 #define GERENCIA_HPP
 
+/**
+*@file Gerencia.hpp
+*@brief Classe Gerencia
+*@author x
+*@version 1.0
+*@date 17/11/2018
+*/
+
 #include "Pessoa.hpp"
 #include "Usuario.hpp"
 #include "funcionario.hpp"
@@ -18,10 +26,39 @@ class Gerencia{
 		vector <Funcionarios> _funcionariosCadastrados;
 		
 	public:
+	
+		/**
+		*@brief construtor de Gerencia
+		*@return Gerencia
+		*/
 		Gerencia();
+	
+		/**
+		*@brief setUsuariosCadastrado
+		*@param novo
+		*/
 		void setUsuarioCadastrado(Usuario);
+	
+		/**
+		*@brief setFuncionarioCadastrado
+		*@param novo
+		*/
 		void setFuncionarioCadastrado(Funcionario);
+	
+		/**
+		*@brief pesquisaFuncionario
+		*@param idCadastro
+		*@param senhaCadastro
+		*@return inteiro
+		*/
 		int pesquisaFuncionario(int,string);
+	
+		/**
+		*@brief pesquisaUsuario
+		*@param idCadastro
+		*@param senhaCadastro
+		*@return inteiro
+		*/
 		int pesquisaUsuario(int,string);
 
 };
