6fb3748f77c557b41c16d67f9fbe62867a3841fe
gabcerqueira <43377719+gabcerqueira@users.noreply.github.com>
2018-11-18 01:50:29 -0200

    Update Item.hpp

--- Item.hpp
+++ Item.hpp
@@ -1,6 +1,15 @@
 #ifndef ITEM_HPP
 #define ITEM_HPP
 
+/**
+*@file Item.hpp
+*@brief Classe Item
+*@author x
+*@version 1.0
+*@date 17/11/2018
+*/
+
+
 #include <stdbool.h>
 #include <string>
 #include <iostrem>
@@ -20,15 +29,61 @@ class Item{
 		
 	public:
 		
+		/**
+		*@brief construtor de Item
+		*@param titulo
+		*@param autor
+		*@param editora
+		*@param disponibilidade
+		*@return objeto
+		*/
 		Item();
 		Item(string,string,string,bool);
+		
+		/**
+		*@brief setTitulo 
+		*@param titulo
+		*/
 		void setTitulo(string);
+		
+		/**
+		*@brief setAutor 
+		*@param autor
+		*/
 		void setAutor(string autor);	
-		void setEditora(string);				
-		string getTitulo();		
-		string getAutor();		
+		
+		/**
+		*@brief setEditora
+		*@param editora
+		*/
+		void setEditora(string);			
+		
+		/**
+		*@brief  getTitulo
+		*@return titulo
+		*/
+		string getTitulo();
+		
+		/**
+		*@brief  getAutor
+		*@return autor
+		*/
+		string getAutor();
+		
+		/**
+		*@brief  getEditora
+		*@return editora
+		*/
 		string getEditora();			
+		
+		/**
+		*@brief  imprimeCampos
+		*/
 		virtual void imprimeCampos();
+		
+		/**
+		*@brief  isDisponivel
+		*/
 		virtual bool isDisponivel();
 
 };
