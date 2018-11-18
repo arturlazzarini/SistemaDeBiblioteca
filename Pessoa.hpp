731b43421aabb7210078d973ab3ef89624fb4f9a
gabcerqueira <43377719+gabcerqueira@users.noreply.github.com>
2018-11-18 02:22:04 -0200

    Update Pessoa.hpp

--- Pessoa.hpp
+++ Pessoa.hpp
@@ -1,6 +1,15 @@
 #ifndef PESSOA_HPP
 #define PESSOA_HPP
 
+
+/**
+*@file Pessoa.hpp
+*@brief Classe Pessoa
+*@author x
+*@version 1.0
+*@date 17/11/2018
+*/
+
 #include <iostream>
 #include <string.h>
 
@@ -14,24 +23,52 @@ class Pessoa{
 		int _id = 0;
 	
 	public:
+	
+	
+		/**
+		*@brief construtor de Pessoa
+		*@param nome
+		*@param senha
+		*@param id
+		*@return objeto
+		*/
 		Pessoa(string, string, int){
 
-	
+		
+		/**
+		*@brief setNome
+		*@param nome
+		*/
 		void setNome(string nome);
 
-		
+		/**
+		*@brief setSenha
+		*@param senha
+		*/
 		void setSenha(string senha);
 
-		
+		/**
+		*@brief setId
+		*@param id
+		*/
 		void setId(int id);
 
-		
+		/**
+		*@brief  getNome 
+		*@return _nome
+		*/
 		string getNome();
 
-		
+		/**
+		*@brief  getSenha
+		*@return _senha
+		*/
 		string getSenha();
 
-		
+		/**
+		*@brief  getId
+		*@return _id
+		*/
 		string getId();
 
 };
