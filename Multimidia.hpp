8e8e2855d62d71248c87f2b9337016637e4ec70d
gabcerqueira <43377719+gabcerqueira@users.noreply.github.com>
2018-11-18 21:17:57 -0200

    Update Multimidia.hpp

--- Multimidia.hpp
+++ Multimidia.hpp
@@ -1,6 +1,14 @@
 #ifndef MULTIMIDIA_HPP
 #define MULTIMIDIA_HPP
 
+/**
+*@file Multimidia.hpp
+*@brief Classe Multimidia
+*@author x
+*@version 1.0
+*@date 17/11/2018
+*/
+
 #include "Item.hpp"
 #include <string>
 
@@ -13,11 +21,34 @@ class Multimidia : public Item{
 		string _tipoMultimidia;
 	
 	public:
-
+	
+		
+		/**
+		*@brief construtor de Multimidia
+		*@param titulo
+		*@param autor
+		*@param editora
+		*@param tipo
+		*@param disponibilidade
+		*@return Multimidia
+		*/
 		Multimidia();
 		Multimidia(string,string,string,string,bool);
+	
+		/**
+		*@brief isDisponivel
+		*@return _disponivel
+		*/
 		bool isDisponivel();
+	
+		/**
+		*@brief mudarDisponibilidade
+		*/
 		void mudarDisponibilidade();
+	
+		/**
+		*@brief imprimeCampos
+		*/
 		void imprimeCampos();
 }
 
