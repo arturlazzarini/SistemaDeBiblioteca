207f6745e6fa1539db737df281d47352795ca6dc
gabcerqueira <43377719+gabcerqueira@users.noreply.github.com>
2018-11-18 02:14:55 -0200

    Update Acervo.hpp

--- Acervo.hpp
+++ Acervo.hpp
@@ -1,6 +1,15 @@
 #ifndef ACERVO_HPP
 #define ACERVO_HPP
 
+
+/**
+*@file Acervo.hpp
+*@brief Classe Acervo
+*@author x
+*@version 1.0
+*@date 17/11/2018
+*/
+
 #include "Livro.hpp"
 #include "Multimidia.hpp"
 #include <vector>
@@ -22,24 +31,116 @@ class Acervo{
 		vector <Multimidia> _acervoMultimidia;
 
 	public:
-
+		
+	
+		/**
+		*@brief construtor de Acervo
+		*@return Acervo
+		*/
 		Acervo();
+	
+		/**
+		*@brief inicializarAcervo
+		*/
 		void inicializarAcervo();
+	
+		/**
+		*@brief adicionarMultimidia
+		*@param multimidia
+		*/
 		void adicionarMultimidia(Multimidia);
+	
+	
+		/**
+		*@brief adicionarLivro
+		*@param livro
+		*/
 		void adicionarLivro(Livro);
+	
+		/**
+		*@brief removerLivro
+		*@param livro
+		*/
 		void removerLivro(Livro);
+	
+		/**
+		*@brief removerMultimidia
+		*@param multimidia
+		*/
 		void removerMultimidia(Multimidia);
+	
+		/**
+		*@brief setAcervoTotal
+		*@param totalLivro
+		*@param totalMultimidia
+		*/
 		void setAcervoTotal(int,int);
+	
+		/**
+		*@brief setAcervoTotal
+		*@param totalLivro
+		*/
 		void setAcervoTotalLivro(int);
+	
+		/**
+		*@brief setAcervoTotalMultimidia
+		*@param totalMultimidia
+		*/
 		void setAcervoTotalMultimidia(int);
+	
+		/**
+		*@brief getAcervoTotal
+		*@return _acervoTotalLivro
+		*@return _acervoTotalMultimidia
+		*/
 		int getAcervoTotal();
+	
+		/**
+		*@brief getAcervoTotalLivro
+		*@return _acervoTotalLivro
+		*/
 		int getAcervoTotalLivro();
-		int getAcervoTotalMuktimidia();
+	
+		/**
+		*@brief getAcervoTotalMultimidia
+		*@return _acervoTotalMultimidia
+		*/
+		int getAcervoTotalMultimidia();
+	
+		/**
+		*@brief listarLivros
+		*/
 		void listarLivros();
+	
+		/**
+		*@brief listarMultimidia
+		*/
 		void listarMultimidia();
+	
+		/**
+		*@brief procurarLivro
+		*@param livro
+		*@param campo
+		*/
 		void procurarLivro(string,int);
+	
+		/**
+		*@brief procurarMultimidia
+		*@param multimidia
+		*@param campo
+		*/
 		void procurarMultimidia(string,int);
+	
+		/**
+		*@brief mudarDisponibilidadeLivro
+		*@param livro
+		*/
 		void mudarDisponibilidadeLivro(Livro);
+	
+		/**
+		*@brief mudarDisponibilidadeMultimidia
+		*@param multimidia
+		*/
 		void mudarDisponibilidadeMultimidia(Multimidia);
 }
 
