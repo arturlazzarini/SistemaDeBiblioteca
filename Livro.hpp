90b400aa31af0511480870e71ace5ac7044b48c8
gabcerqueira <43377719+gabcerqueira@users.noreply.github.com>
2018-11-18 02:29:22 -0200

    Update Livro.hpp

--- Livro.hpp
+++ Livro.hpp
@@ -1,6 +1,15 @@
 #ifndef LIVRO_HPP
 #define LIVRO_HPP
 
+/**
+*@file Livro.hpp
+*@brief Classe Livro
+*@author x
+*@version 1.0
+*@date 17/11/2018
+*/
+
+
 #include "Item.hpp"
 
 class Livro : public Item{
@@ -9,11 +18,33 @@ class Livro : public Item{
 		bool _disponibilidade = true;
 
 	public:
-
+		
+	
+		/**
+		*@brief construtor de Livro
+		*@param titulo
+		*@param autor
+		*@param editora
+		*@param disponibilidade
+		*@return Livro
+		*/
 		Livro();
 		Livro(string,string,string,bool);
+	
+		/**
+		*@brief isDisponivel
+		*@return _disponivel
+		*/
 		bool isDisponivel();
+	
+		/**
+		*@brief mudarDisponibilidade
+		*/
 		void mudarDisponibilidade();
+	
+		/**
+		*@brief imprimeCampos
+		*/
 		void imprimeCampos();
 }
 
