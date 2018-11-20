9a616c533a0ca0dc005d56db311da4d25fb7db4c
arturlazzarini <42216694+arturlazzarini@users.noreply.github.com>
2018-11-20 16:00:38 -0200

    testes

---
+++ testegerencia.cpp
@@ -0,0 +1,31 @@
+#include "doctest.h"
+
+#include "Gerencia.hpp"
+#include "Pessoa.hpp"
+#include "Usuario.hpp"
+#include "Funcionario.hpp"
+
+TEST_CASE("Testa cadastro e pesquisa de usuarios"){
+
+	Gerencia atual;
+
+	Usuario novo("Admin", "0000", 0000);
+
+	if(atual.pesquisaUsuario(0000, "0000") == 0){
+		atual.setUsuarioCadastrado(novo);
+		CHECK(atual.pesquisaUsuario(0000, "0000") == 1);
+	}
+}
+
+TEST_CASE("Testa cadastro e pesquisa de funcionario"){
+
+	Gerencia atual;
+
+	Funcionario novo("Admin", "0000", 0000);
+
+	if(atual.pesquisaFuncionario(0000, "0000") == 0){
+		atual.setFuncionarioCadastrado(novo);
+		CHECK(atual.pesquisaFuncionario(0000, "0000") == 1);
+	}
+
+}
---
+++ testeusuario.cpp
@@ -0,0 +1,54 @@
+#include "doctest.h"
+
+#include "Pessoa.hpp"
+#include "Usuario.hpp"
+#include "Item.cpp"
+#include "Livro.hpp"
+#include "Multimidia.hpp"
+
+TEST_CASE("Testa Construtor"){
+
+	Usuario novo;
+	novo.setNome("Admin");
+	novo.setSenha("0000");
+	novo.setId(0000);
+
+	if(novo.getNome() == "Admin" && novo.getSenha() == "0000"){
+		CHECK(novo.getId() == 0000);
+	}
+
+}
+
+TEST_CASE("Testa funcoes com livro"){
+
+	Usuario novo("Admin", "0000", 0000);
+
+	Livro livro("Guerra e Paz", "Liev Tolstoi", "Globo", true);
+
+	novo.reservarLivro(livro);
+	novo.alugarLivro(livro);
+	livro.mudarDisponibilidade();
+	if(livro.isDisponivel() == false){
+		novo.devolverLivro("Guerra e Paz");
+		livro.mudarDisponibilidade();
+		CHECK(livro.isDisponivel() == true);
+	}
+
+}
+
+TEST_CASE("Testa funcoes com multimidia"){
+
+	Usuario novo("Admin", "0000", 0000);
+
+	Multimidia multi("O Poderoso Chefao", "Francis Ford Coppola", "Paramount", "Filme", true);
+
+	novo.reservarMultimidia(multi);
+	novo.alugarMultimidia(multi);
+	multi.mudarDisponibilidade();
+	if(multi.isDisponivel() == false){
+		novo.devolverMultimidia("O Poderoso Chefao");
+		multi.mudarDisponibilidade();
+		CHECK(livro.isDisponivel() == true);
+	}
+
+}
