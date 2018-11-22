845fdffb3aaff5fa70d4552cda1a1e09988755b5
arthurrlisboa <42387780+arthurrlisboa@users.noreply.github.com>
2018-11-22 00:31:04 -0200

    Update Acervo.hpp

--- Acervo.hpp
+++ Acervo.hpp
@@ -1,15 +1,6 @@
 #ifndef ACERVO_HPP
 #define ACERVO_HPP
 
-
-/**
-*@file Acervo.hpp
-*@brief Classe Acervo
-*@author x
-*@version 1.0
-*@date 17/11/2018
-*/
-
 #include "Livro.hpp"
 #include "Multimidia.hpp"
 #include <vector>
@@ -17,10 +8,13 @@
 #include <fstream>
 #include <cstdlib>
 #include <iomanip>
+#include <string>
+#include <sstream>
 
 using std::vector;
 using std::string;
-using std::cout;
+using std::stringstream;
+using std::ifstream;
 
 class Acervo{
 	
@@ -31,117 +25,27 @@ class Acervo{
 		vector <Multimidia> _acervoMultimidia;
 
 	public:
-		
-	
-		/**
-		*@brief construtor de Acervo
-		*@return Acervo
-		*/
+
 		Acervo();
-	
-		/**
-		*@brief inicializarAcervo
-		*/
 		void inicializarAcervo();
-	
-		/**
-		*@brief adicionarMultimidia
-		*@param multimidia
-		*/
 		void adicionarMultimidia(Multimidia);
-	
-	
-		/**
-		*@brief adicionarLivro
-		*@param livro
-		*/
 		void adicionarLivro(Livro);
-	
-		/**
-		*@brief removerLivro
-		*@param livro
-		*/
 		void removerLivro(Livro);
-	
-		/**
-		*@brief removerMultimidia
-		*@param multimidia
-		*/
 		void removerMultimidia(Multimidia);
-	
-		/**
-		*@brief setAcervoTotal
-		*@param totalLivro
-		*@param totalMultimidia
-		*/
 		void setAcervoTotal(int,int);
-	
-		/**
-		*@brief setAcervoTotal
-		*@param totalLivro
-		*/
 		void setAcervoTotalLivro(int);
-	
-		/**
-		*@brief setAcervoTotalMultimidia
-		*@param totalMultimidia
-		*/
 		void setAcervoTotalMultimidia(int);
-	
-		/**
-		*@brief getAcervoTotal
-		*@return _acervoTotalLivro
-		*@return _acervoTotalMultimidia
-		*/
 		int getAcervoTotal();
-	
-		/**
-		*@brief getAcervoTotalLivro
-		*@return _acervoTotalLivro
-		*/
 		int getAcervoTotalLivro();
-	
-		/**
-		*@brief getAcervoTotalMultimidia
-		*@return _acervoTotalMultimidia
-		*/
 		int getAcervoTotalMultimidia();
-	
-		/**
-		*@brief listarLivros
-		*/
 		void listarLivros();
-	
-		/**
-		*@brief listarMultimidia
-		*/
 		void listarMultimidia();
-	
-		/**
-		*@brief procurarLivro
-		*@param livro
-		*@param campo
-		*/
-		void procurarLivro(string,int);
-	
-		/**
-		*@brief procurarMultimidia
-		*@param multimidia
-		*@param campo
-		*/
-		void procurarMultimidia(string,int);
-	
-		/**
-		*@brief mudarDisponibilidadeLivro
-		*@param livro
-		*/
+		void procuraLivro(string,int);
+		void procuraMultimidia(string,int);
+		int pesquisaLivro(Livro);
+		int pesquisaMultimidia(Multimidia);
 		void mudarDisponibilidadeLivro(Livro);
-	
-		/**
-		*@brief mudarDisponibilidadeMultimidia
-		*@param multimidia
-		*/
-		void mudarDisponibilidadeMultimidia(Multimidia);
-}
+		void mudarDisponibilidadeMultimidia(Multimidia);	
+};
 
 #endif
