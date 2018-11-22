f122a7da4ec03ac0389a57af45d8613e51fad614
arthurrlisboa <42387780+arthurrlisboa@users.noreply.github.com>
2018-11-22 00:42:50 -0200

    Update Usuario.hpp

--- Usuario.hpp
+++ Usuario.hpp
@@ -1,99 +1,38 @@
 #ifndef USUARIO_HPP
 #define USUARIO_HPP
 
-
-/**
-*@file Usuario.hpp
-*@brief Classe Usuario
-*@author x
-*@version 1.0
-*@date 18/11/2018
-*/
-
-
 #include "Pessoa.hpp"
 #include "Item.hpp"
+#include "Multimidia.hpp"
+#include "Livro.hpp"
 #include <vector>
+
+using std::vector;
 
 class Usuario : public Pessoa{
 
 	private:
 
-		vector<Livros> _livrosAlugados;
+		vector<Livro> _livrosAlugados;
 		vector<Multimidia> _multimidiaAlugados;
-		vector<Livros> _livrosReservados;
+		vector<Livro> _livrosReservados;
 		vector<Multimidia> _multimidiaReservados;
 
 	public:
-		/**
-		*@brief construtor de Usuario
-		*@param nome
-		*@param senha
-		*@param id
-		*@return objeto
-		*/	
+
 		Usuario();
-		Usuario(string,string,id);
-	
-		/**
-		*@brief alugarLivro
-		*@param item
-		*/
+		Usuario(string,string,int);
 		void alugarLivro(Livro);
-	
-		/**
-		*@brief reservarLivro
-		*@param item
-		*/
 		void reservarLivro(Livro);
-	
-		/**
-		*@brief devolverLivro
-		*@param item
-		*/
 		void devolverLivro(string);
-	
-		/**
-		*@brief alugarMultimidia
-		*@param item
-		*/
 		void alugarMultimidia(Multimidia);
-	
-		/**
-		*@brief reservarMultimidia
-		*@param item
-		*/
 		void reservarMultimidia(Multimidia);
-	
-		/**
-		*@brief devolverMultimidia
-		*@param item
-		*/
 		void devolverMultimidia(string);
-	
-		/**
-		*@brief imprimeLivrosAlugados
-		*@return livros alugados
-		*/
 		void imprimeLivrosAlugados();
-	
-		/**
-		*@brief imprimeLivrosReservados
-		*@return livros reservados
-		*/
 		void imprimeLivrosReservados();
-	
-		/**
-		*@brief imprimeMultimidiaAlugados
-		*@return multimidias alugadas
-		*/
 		void imprimeMultimidiaAlugados();
-	
-		/**
-		*@brief imprimeMultimidiaReservados
-		*@return multimidias reservadas
-		*/
 		void imprimeMultimidiaReservados();
-}
+		void atribuiItens(Usuario*);
+};
 
 #endif
