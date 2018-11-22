d144bbb6fdb9675272db860e7b8d69ded24c7721
arthurrlisboa <42387780+arthurrlisboa@users.noreply.github.com>
2018-11-22 00:39:41 -0200

    Update Multimidia.hpp

--- Multimidia.hpp
+++ Multimidia.hpp
@@ -1,55 +1,23 @@
 #ifndef MULTIMIDIA_HPP
 #define MULTIMIDIA_HPP
 
-/**
-*@file Multimidia.hpp
-*@brief Classe Multimidia
-*@author x
-*@version 1.0
-*@date 17/11/2018
-*/
-
 #include "Item.hpp"
-#include <string>
-
-using std::string;
-using std::cout;
 
 class Multimidia : public Item{
 	
 	private:
+		bool _disponibilidade = true;
 		string _tipoMultimidia;
 	
 	public:
-	
-		
-		/**
-		*@brief construtor de Multimidia
-		*@param titulo
-		*@param autor
-		*@param editora
-		*@param tipo
-		*@param disponibilidade
-		*@return Multimidia
-		*/
+
 		Multimidia();
 		Multimidia(string,string,string,string,bool);
-	
-		/**
-		*@brief isDisponivel
-		*@return _disponivel
-		*/
+		void setTipo(string);
+		string getTipo();
 		bool isDisponivel();
-	
-		/**
-		*@brief mudarDisponibilidade
-		*/
 		void mudarDisponibilidade();
-	
-		/**
-		*@brief imprimeCampos
-		*/
 		void imprimeCampos();
-}
+};
 
 #endif
