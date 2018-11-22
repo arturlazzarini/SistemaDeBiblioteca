5c5b1b586311554e25bb25cb5eceffb46d010210
arthurrlisboa <42387780+arthurrlisboa@users.noreply.github.com>
2018-11-22 00:38:42 -0200

    Update Livro.hpp

--- Livro.hpp
+++ Livro.hpp
@@ -1,15 +1,6 @@
 #ifndef LIVRO_HPP
 #define LIVRO_HPP
 
-/**
-*@file Livro.hpp
-*@brief Classe Livro
-*@author x
-*@version 1.0
-*@date 17/11/2018
-*/
-
-
 #include "Item.hpp"
 
 class Livro : public Item{
@@ -18,34 +9,13 @@ class Livro : public Item{
 		bool _disponibilidade = true;
 
 	public:
-		
-	
-		/**
-		*@brief construtor de Livro
-		*@param titulo
-		*@param autor
-		*@param editora
-		*@param disponibilidade
-		*@return Livro
-		*/
+
 		Livro();
 		Livro(string,string,string,bool);
-	
-		/**
-		*@brief isDisponivel
-		*@return _disponivel
-		*/
 		bool isDisponivel();
-	
-		/**
-		*@brief mudarDisponibilidade
-		*/
 		void mudarDisponibilidade();
-	
-		/**
-		*@brief imprimeCampos
-		*/
 		void imprimeCampos();
-}
+};
 
 #endif
+
