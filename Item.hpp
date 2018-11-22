ddba8248509f704804de22c1281c8671103aa5e2
arthurrlisboa <42387780+arthurrlisboa@users.noreply.github.com>
2018-11-22 00:37:18 -0200

    Update Item.hpp

--- Item.hpp
+++ Item.hpp
@@ -1,22 +1,11 @@
 #ifndef ITEM_HPP
 #define ITEM_HPP
 
-/**
-*@file Item.hpp
-*@brief Classe Item
-*@author x
-*@version 1.0
-*@date 17/11/2018
-*/
-
-
 #include <stdbool.h>
-#include <string>
-#include <iostrem>
+#include <string.h>
+#include <iostream>
 
-using std::cout
-using std::cin
-using std::string
+using namespace std;
 
 class Item{
 
@@ -29,61 +18,15 @@ class Item{
 		
 	public:
 		
-		/**
-		*@brief construtor de Item
-		*@param titulo
-		*@param autor
-		*@param editora
-		*@param disponibilidade
-		*@return objeto
-		*/
 		Item();
 		Item(string,string,string,bool);
-		
-		/**
-		*@brief setTitulo 
-		*@param titulo
-		*/
 		void setTitulo(string);
-		
-		/**
-		*@brief setAutor 
-		*@param autor
-		*/
 		void setAutor(string autor);	
-		
-		/**
-		*@brief setEditora
-		*@param editora
-		*/
-		void setEditora(string);			
-		
-		/**
-		*@brief  getTitulo
-		*@return titulo
-		*/
-		string getTitulo();
-		
-		/**
-		*@brief  getAutor
-		*@return autor
-		*/
-		string getAutor();
-		
-		/**
-		*@brief  getEditora
-		*@return editora
-		*/
+		void setEditora(string);
+		string getTitulo();		
+		string getAutor();		
 		string getEditora();			
-		
-		/**
-		*@brief  imprimeCampos
-		*/
 		virtual void imprimeCampos();
-		
-		/**
-		*@brief  isDisponivel
-		*/
 		virtual bool isDisponivel();
 
 };
