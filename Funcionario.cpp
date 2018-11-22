f4dcb1b41bc7a584fe2c723b904668068a794748
arthurrlisboa <42387780+arthurrlisboa@users.noreply.github.com>
2018-11-22 00:33:47 -0200

    Update Funcionario.cpp

--- Funcionario.cpp
+++ Funcionario.cpp
@@ -4,43 +4,39 @@ Funcionario :: Funcionario(){}
 
 Funcionario :: Funcionario(string nome, string senha, int id) : Pessoa(nome,senha,id){}
 
-//Adiciona novos livros ao acervo da biblioteca
-void Funcionario::adicionarlivros(Acervo *acervo,string titulo, string autor, string editora){
+void Funcionario::adicionarLivros(Acervo *acervo,string titulo, string autor, string prod){
 	
-	Livro novo(titulo,autor,editora,true);
-	acervo->adicionarlivro(novo);
+	Livro novo(titulo,autor,prod,true);
+	acervo->adicionarLivro(novo);
 }
 
-//Remove novos livros do acervo da biblioteca
-void Funcionario::removerLivros(Acervo *acervo,string titulo, string autor, string editora){
+void Funcionario::removerLivros(Acervo *acervo,string titulo, string autor, string prod){
 	
-	Livro livro(titulo,autor, editora,true);
+	Livro livro(titulo,autor, prod,true);
 	acervo->removerLivro(livro);
 }
-//Adiciona novas multimidias ao acervo da biblioteca
-void Funcionario::adicionarMultimidias(Acervo *acervo,string titulo, string autor, string editora, string tipo){
+
+void Funcionario::adicionarMultimidias(Acervo *acervo,string titulo, string autor, string produtora, string tipo){
 	
-	Multimidia nova(titulo,autor,editora,tipo,true);
+	Multimidia nova(titulo,autor,produtora,tipo,true);
 	acervo->adicionarMultimidia(nova);
 }
 
-//Remove multimidia do acervo da biblioteca
-void Funcionario::removerMultimidias(Acervo *acervo,string titulo, string autor,string editora, string tipo){
+
+void Funcionario::removerMultimidias(Acervo *acervo,string titulo, string autor,string produtora, string tipo){
 	
-	Multimidia retirada(titulo,autor,editora,tipo,true);
+	Multimidia retirada(titulo,autor,produtora,tipo,true);
 	acervo->adicionarMultimidia(retirada);
 }
 
-//Cadastra novos usuários e os adiciona à gerencia
-void Funcionario::cadastrarUsuario(Gerencia *adm, string nome, string senha, int id){
+Usuario Funcionario::cadastrarUsuario( string nome, string senha, int id){
 	
 	Usuario novo(nome, senha, id);
-	adm->setUsuarioCadastrado(novo);
+	return novo;
 }
 
-//Cadastra novos usuários e os adiciona à gerencia
-void Funcionario::cadastrarFuncionario(Gerencia *adm, string nome, string senha, int id){
+Funcionario Funcionario::cadastrarFuncionario( string nome, string senha, int id){
 	
 	Funcionario novo(nome, senha, id);
-	adm->setFuncionarioCadastrado(novo);
+	return novo;
 }
